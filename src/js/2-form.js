c1d30992012e45b3360dc0589fbfbeb8c06983ba
Helenakhudsk <125953044+Helenakhudsk@users.noreply.github.com>
2024-05-09 20:36:14 +0300

    update  index.html

---
+++ src/js/2-form.js
@@ -0,0 +1,29 @@
+    const form = document.querySelector('.feedback-form');
+    const input = document.querySelector('input[type="email"]');
+    const textarea = document.querySelector('textarea');
+
+    let formDate = {
+        email: "",
+        message: "",
+    };
+
+form.addEventListener('input', event => {
+    event.preventDefault();
+    formDate.email = input.value.trim();
+    formDate.message = textarea.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(formDate));
+});
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    if(formDate.email === "" || formDate.message === ""){
+        alert('Fill please all fields')
+    }
+    else{
+        console.log(formDate);
+        localStorage.removeItem('feedback-form-state');
+        form.reset();
+        formDate = {email: "", message: "",}
+    }
+}
+);
